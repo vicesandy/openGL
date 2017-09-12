1d952a4d7abd32765a45ab9c0e3af2c9ed66a7f0
Junchao Chen <csammy@siu.edu>
2017-09-12 08:37:34 -0500

    lab4

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab4
+//
+//  Created by Junchao Chen on 9/12/17.
+//  Copyright © 2017 Junchao Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
